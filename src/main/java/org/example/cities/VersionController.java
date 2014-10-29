9c9922601e7614e7fc9ef1d22a356974cf9df95f
acrlewis <acrlewis1@gmail.com>
2014-10-29 09:16:53 -0700

    I am Awesome

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings London Roadshow!!";
+        return "Greetings London Roadshow!!!";
     }
 }
